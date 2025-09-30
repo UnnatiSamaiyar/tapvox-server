cdfd957646f61eca463a2ac20371b8df2bddbc84
UnnatiSamaiyar <unnatisamaiyar02@gmail.com>
2025-09-30 11:10:37 +0530

    port update

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const blogRoute = require('./routes/blogRoutes');
 
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5002;
 
 // Middlewares
 // Middlewares
